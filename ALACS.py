86c70cdd79bbb961d525d2ccbaa7a767b2bd91de
Zenahr <zenmatica@gmail.com>
2021-09-29 06:48:46 +0200

    threading WIP

--- ALACS.py
+++ ALACS.py
@@ -5,6 +5,8 @@ from lib import click_readyup_button
 import json
 from command_runner.elevate import elevate
 from time import sleep
+from threading import Thread
+import keyboard
 
 ACTIVATION_BUTTON = json.load(open('./config.json'))['key']
 
@@ -15,28 +17,24 @@ def update_should_run():
     SHOULD_RUN = not SHOULD_RUN        
     print(SHOULD_RUN)
 
+thread = Thread(target=click_readyup_button, daemon=True)
+
 def main():
     print('MAKE SURE TO CLOSE THIS WINDOW AFTER CLOSING APEX')
     print('TO CHANGE THE ACTIVATION KEY READ THE INSTRUCTIONS FOUND IN instructions.txt')
     print('ACTIVATION KEY IS SET TO:', '>>> ', ACTIVATION_BUTTON, ' <<<')
     print('LISTENING FOR ACTIVATION KEY ...')
 
-    def on_press(key):
-                if key == keyboard.Key[ACTIVATION_BUTTON]:
-                    update_should_run()
-
-    def on_release(key):
-        pass
-
-    with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
-
     while True:
-        # if SHOULD_RUN:
+        if SHOULD_RUN:
             sleep(1)
-            click_readyup_button()
+            if not thread.is_alive():
+                thread.start()
+        elif not SHOULD_RUN:
+            if thread.is_alive():
+                thread.join()
+        if keyboard.is_pressed('b') == True:
+            update_should_run()
 
 
 if __name__ == '__main__':
